b3a6f4c808912199d4ffb3ac4ddf8e8fc80bbec3
Karn Tippimol <zkarn.t@pttdigital.com>
2025-03-27 18:36:50 +0700

    - update SEO

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 export default function Home() {
+
   return (
     <><h1 className="text-3xl">Home Page</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus ipsa placeat vitae voluptatibus similique, illo rem ab? Odio iusto officia, ab facilis fugit, debitis autem ratione blanditiis voluptatem, excepturi quasi?</p>
